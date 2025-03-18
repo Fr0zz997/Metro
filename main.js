46d715968cd3cc76f92afb8b19d92dfc1a3c33e3
Fr0zz997 <86337479+Fr0zz997@users.noreply.github.com>
2025-03-18 12:34:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.hero__container');
+ScrollReveal().reveal('.banner__container');
+ScrollReveal().reveal('.news__container');
+ScrollReveal().reveal('.slider');
+ScrollReveal().reveal('.footer');
